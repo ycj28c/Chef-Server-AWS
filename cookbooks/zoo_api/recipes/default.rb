b8627de7ccfff29636f86ced4a5378054eccff9b
ycj28c <ycj28c@gmail.com>
2017-12-07 01:31:55 +0000

    add the zoo deployment cookbook, register command as a service

--- cookbooks/zoo_api/recipes/default.rb
+++ cookbooks/zoo_api/recipes/default.rb
@@ -16,4 +16,12 @@ git "/opt/zoo_api" do
   action :sync
 end
 
+remote_file '/opt/zoo_api/bootzoo.jar' do
+  source 'https://downloads.sourceforge.net/project/springboot-zoo-demo/bootZoo-0.0.1-SNAPSHOT.jar?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fspringboot-zoo-demo%2F%3Fsource%3Dnavbar&ts=1512600396&use_mirror=cfhcable'
+  owner 'cool_user'
+  mode '0755'
+end
 
+service 'bootzoo' do
+  action [:enable, :start]
+end
